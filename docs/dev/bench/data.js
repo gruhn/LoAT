4a6d5cdbb0412fac7c31867a3fd873cb1ce1aaaa
github-action-benchmark <github@users.noreply.github.com>
2024-03-16 23:14:00 +0000

    add LoAT Benchmarks (customBiggerIsBetter) benchmark result for fa6f11a5d28a3dc60cce52ace82b6e0a878b6af3

--- docs/dev/bench/data.js
+++ docs/dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710627656325,
+  "lastUpdate": 1710630840906,
   "repoUrl": "https://github.com/gruhn/LoAT",
   "entries": {
     "LoAT Benchmarks": [
@@ -95,6 +95,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "chc-comp22-benchmarks timeout",
             "value": "279",
+            "unit": "number"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Niklas Gruhn",
+            "username": "gruhn",
+            "email": "niklas@gruhn.me"
+          },
+          "committer": {
+            "name": "Niklas Gruhn",
+            "username": "gruhn",
+            "email": "niklas@gruhn.me"
+          },
+          "id": "fa6f11a5d28a3dc60cce52ace82b6e0a878b6af3",
+          "message": "temp CI customization",
+          "timestamp": "2024-03-16T15:20:11Z",
+          "url": "https://github.com/gruhn/LoAT/commit/fa6f11a5d28a3dc60cce52ace82b6e0a878b6af3"
+        },
+        "date": 1710630839276,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "chc-comp23-benchmarks sat",
+            "value": "42",
+            "unit": "number"
+          },
+          {
+            "name": "chc-comp23-benchmarks unknown",
+            "value": "27",
+            "unit": "number"
+          },
+          {
+            "name": "chc-comp23-benchmarks unsat",
+            "value": "69",
+            "unit": "number"
+          },
+          {
+            "name": "chc-comp23-benchmarks timeout",
+            "value": "290",
             "unit": "number"
           }
         ]

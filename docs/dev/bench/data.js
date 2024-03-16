99c03b89771797aa0ed1ab17f338e6beb6f5b434
github-action-benchmark <github@users.noreply.github.com>
2024-03-16 15:22:37 +0000

    add LoAT Benchmarks (customBiggerIsBetter) benchmark result for 3206445b92fe60a723ee1704933a915ebd8eb86f

---
+++ docs/dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1710602557039,
+  "repoUrl": "https://github.com/gruhn/LoAT",
+  "entries": {
+    "LoAT Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "Niklas Gruhn",
+            "username": "gruhn",
+            "email": "niklas@gruhn.me"
+          },
+          "committer": {
+            "name": "Niklas Gruhn",
+            "username": "gruhn",
+            "email": "niklas@gruhn.me"
+          },
+          "id": "3206445b92fe60a723ee1704933a915ebd8eb86f",
+          "message": "ci: add benchmark options: LIA, LIA-nonlin",
+          "timestamp": "2024-03-16T15:20:11Z",
+          "url": "https://github.com/gruhn/LoAT/commit/3206445b92fe60a723ee1704933a915ebd8eb86f"
+        },
+        "date": 1710602555304,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "chc-comp22-benchmarks error",
+            "value": "456",
+            "unit": "number"
+          }
+        ]
+      }
+    ]
+  }
+}

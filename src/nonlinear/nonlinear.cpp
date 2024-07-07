35e6411a688b562803cab43d89fec649a61b1cf4
Niklas Gruhn <niklas@gruhn.me>
2024-07-07 18:47:42 +0200

    Update nonlinear.cpp

--- src/nonlinear/nonlinear.cpp
+++ src/nonlinear/nonlinear.cpp
@@ -46,13 +46,9 @@ void NonLinearSolver::analyze(const std::vector<Clause>& initial_chcs) {
         std::cout << "sat" << std::endl;
         return;    
     } else if (allLinear(chcs_preprocessed)) {
-        // ITSPtr its = ITSProblem::fromClauses(chcs_preprocessed);
-        // reachability::Reachability::analyze(*its);
-        std::cout << "lin" << std::endl;
+        ITSPtr its = ITSProblem::fromClauses(chcs_preprocessed);
+        reachability::Reachability::analyze(*its);
         return;  
-    } else {
-        std::cout << "nonlin" << std::endl;
-        return; 
     }
 
     auto [linear_chcs, non_linear_chcs] = partitionByDegree(chcs_preprocessed);
